447bb7abaa812c9967460075246c83bc3d8d69d8
thismarvin <bend_@outlook.com>
2019-11-18 18:22:43 -0600

    first pass at new histogram graph

--- src/js/modules/graphing.js
+++ src/js/modules/graphing.js
@@ -1,5 +1,6 @@
 const p5 = require("p5");
 
+//#region Line Graph
 class GraphData {
     constructor(id, data, thickness, zIndex, color) {
         this.id = id;
@@ -114,3 +115,145 @@ class LineGraph {
 }
 
 module.exports.LineGraph = LineGraph;
+
+//#endregion
+
+class HistogramData {
+    constructor(id, data, zIndex, color) {
+        this.id = id;
+        this.data = data;
+        this.zIndex = zIndex;
+        this.color = color;
+    }
+}
+
+module.exports.HistogramData = HistogramData;
+
+class Histogram {
+    constructor(id, entries) {
+        this.id = id;
+        this.entries = entries;
+
+        this.entries.sort((a, b) => {
+            return a.zIndex - b.zIndex;
+        });
+
+        this.minimum = 0;
+        this.maximum = 0;
+        this.range = 0;
+
+        this.analyzeData();
+
+        this.padding = 2;
+        this.updateGraph = true;
+
+        const attachedDiv = document.querySelector(`#${id}`);
+        const instance = this;
+        const sketch = (p5) => {
+
+            let myCanvas;
+
+            p5.setup = function () {
+                myCanvas = p5.createCanvas(attachedDiv.clientWidth, attachedDiv.clientHeight);
+                myCanvas.parent(attachedDiv);
+
+                p5.frameRate(60);
+            };
+
+            p5.draw = function () {
+                p5.drawGraph();
+            };
+
+            p5.windowResized = function () {
+                myCanvas = p5.resizeCanvas(0, 0);
+                setTimeout(() => {
+                    myCanvas = p5.resizeCanvas(attachedDiv.clientWidth, attachedDiv.clientHeight);
+                    instance.updateGraph = true;
+                }, 250);
+            }
+
+            p5.drawGraph = function () {
+                if (!instance.updateGraph || instance.maximum === undefined) {
+                    return;
+                }
+
+                p5.clear(0);
+                instance.updateGraph = false;
+
+
+                const start = p5.map(0, instance.minimum, instance.maximum, p5.height - instance.padding, instance.padding);
+
+                let histogramWidth;
+                let histogramHeight;
+
+                p5.noStroke();
+
+                for (let i = 0; i < instance.entries.length; i++) {
+
+                    p5.fill(instance.entries[i].color);
+
+                    histogramWidth = p5.width / instance.entries[i].data.length;
+
+                    for (let j = 0; j < instance.entries[i].data.length; j++) {
+                        if (instance.entries[i].data[j] === undefined) {
+                            continue;
+                        }
+
+                        if (instance.entries[i].data[j] >= 0){
+                            histogramHeight = p5.map(instance.entries[i].data[j], 0, instance.maximum, 0, (p5.height - instance.padding) - start);
+                            p5.fill(0, 255, 0);
+                            p5.rect(
+                                j * histogramWidth,
+                                start - histogramHeight,
+                                histogramWidth,
+                                histogramHeight
+                            );
+                        }
+                        else {
+                            histogramHeight = p5.map(instance.entries[i].data[j], instance.minimum, 0, start, 0);
+                            p5.fill(255, 0, 0);
+                            p5.rect(
+                                j * histogramWidth,
+                                start,
+                                histogramWidth,
+                                histogramHeight
+                            );
+                        }
+                        
+                    }
+                }
+            }
+        };
+
+        let myp5 = new p5(sketch, id);
+    }
+
+    analyzeData() {
+        this.findRange();
+    }
+
+    findRange() {
+        this.minimum = this.entries[0].data[0];
+        this.maximum = this.entries[0].data[0];
+
+        for (let i = 0; i < this.entries.length; i++) {
+            for (let j = 0; j < this.entries[i].data.length; j++) {
+                if (this.entries[i].data[j] === undefined) {
+                    continue;
+                }
+                this.minimum = this.entries[i].data[j] < this.minimum ? this.entries[i].data[j] : this.minimum;
+                this.maximum = this.entries[i].data[j] > this.maximum ? this.entries[i].data[j] : this.maximum;
+            }
+        }
+
+        this.range = this.maximum - this.minimum;
+    }
+
+    updateData(entries) {
+        this.updateGraph = true;
+        this.entries = entries;
+        this.analyzeData();
+    }
+}
+
+module.exports.Histogram = Histogram;
